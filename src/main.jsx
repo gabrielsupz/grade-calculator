3d09d18a3241ded5d6d07b348b7e53f8d7b264ee
Gabriel Suptitz <gabrielsuptitz@gmail.com>
2022-12-22 11:22:35 -0300

    add context, ajeitando animação e NoticeDiv

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './pages/Home/Home'
+import { AuthProvider } from './providers/auth'
 import './styles/global.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <AuthProvider>
+      <Home />
+    </AuthProvider>
   </React.StrictMode>
 )
